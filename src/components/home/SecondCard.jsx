b066c9480dded294dfac21494b6eb0255519df08
Herold-KA <karlmerzi@gmail.com>
2023-02-01 15:26:54 +0100

    infinite scrolling good but can be better; No responsive

--- src/components/home/SecondCard.jsx
+++ src/components/home/SecondCard.jsx
@@ -2,18 +2,38 @@ import {useState, useEffect} from 'react'
 import './SecondCard.css'
 
 const SecondCard = () => {
-  
+  const [nb, setnb] = useState(15)
   const [secondCardImg, setSecondCardImg] = useState([''])
   useEffect(() => {
     request()
+    window.addEventListener('scroll', handleScroll, {
+      passive: true
+    });
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [])
 
+  useEffect(() => {
+    console.log('bir')
+    fetch(`http://localhost:3000/article/nbArticlePageArticle/${nb}`,{method:'GET'})
+        .then(res => res.json())
+        .then(data => {
+            setSecondCardImg(data.articles)
+            console.log(nb)
+        })
+        .catch(err => console.log(err))
+    return () => {
+    };
+  }, [nb,])
 
-  const handleScroll = (e) => {
-    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+  const handleScroll = () => {
+    
+    const bottom = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight
     if (bottom) {
-      alert('lol')
       console.log('lol')
+      setnb(nb + 15)
+      console.log(nb)
      }
   }
     const request = () =>{
@@ -25,7 +45,7 @@ const SecondCard = () => {
         .catch(err => console.log(err))
     }
   return (
-    <div className='divSecondCard' onScroll={() => handleScroll()}>
+    <div className='divSecondCard' onScroll={ handleScroll}>
       <div className="divSecondCardImage" >
         {
           secondCardImg.map((item, index) => (
